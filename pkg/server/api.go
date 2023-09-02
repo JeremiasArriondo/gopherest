cb999533517ccac0774e3786b26bd21984e6e6da
Jeremias Arriondo <jeremiasarriondo98@gmail.com>
2023-09-02 20:59:26 -0300

    feat: methods created

--- pkg/server/api.go
+++ pkg/server/api.go
@@ -1,13 +1,16 @@
 package server
 
 import (
+	"encoding/json"
 	"net/http"
 
+	gopher "github.com/JeremiasArriondo/gopherest/pkg/models"
 	"github.com/gorilla/mux"
 )
 
 type api struct {
-	router http.Handler
+	router     http.Handler
+	repository gopher.GopherRepository
 }
 
 type Server interface {
@@ -18,10 +21,33 @@ func New() Server {
 	a := &api{}
 
 	r := mux.NewRouter()
+	r.HandleFunc("/gophers", a.fetchGophers).Methods(http.MethodGet)
+	r.HandleFunc("/gophers/{ID:[a-zA-Z0-9_]+}", a.fetchGopher).Methods(http.MethodGet)
+
 	a.router = r
 	return a
 }
 
 func (a *api) Router() http.Handler {
 	return a.router
+}
+
+func (a *api) fetchGophers(w http.ResponseWriter, r *http.Request) {
+	gophers, _ := a.repository.FetchGophers()
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(gophers)
+}
+
+func (a *api) fetchGopher(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	gopher, err := a.repository.FetchGopherByID(vars["ID"])
+	w.Header().Set("Content-Type", "application/json")
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound) // We use not found for simplicity
+		json.NewEncoder(w).Encode("Gopher Not found")
+		return
+	}
+
+	json.NewEncoder(w).Encode(gopher)
 }
